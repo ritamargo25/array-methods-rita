739d6ad7afe5f2243eefdf1514ba4348816b9574
ritamargo25 <daisypearl@mail.ru>
2023-03-14 01:22:18 +0300

    init commit

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
-const filterByAge = (el) => {
-
+const filterByAge = (candidate) => {
+    return candidate.age <= 50
 };
 
-const filterByScore = (el) => {
-
+const filterByScore = (candidate) => {
+    return candidate.score >= 75
 };
 
-const filterByServiceRecord = (el) => {
-
+const filterByServiceRecord = (candidate) => {
+    return candidate.serviceRecord >= 5
 };
 
 const sortByScore = (arr) => {
-
+    return arr.score((a, b) => b.score - a.score);
 };
 
 const getMeanAge = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+console.log(getMeanAge([60, 35, 29, 45, 54]));
 
-};
 
-const getMeanAgeReduce = (arr) => {
-
+const getMeanAgeReduce = (candidates) => {
+    const sum = candidates.reduce((a, b) => a + b.age, 0);
+    return sum / candidates.length;
 };
 
 const getNamesOnly = (arr) => {
-
+    return arr.name;
 };
 
 module.exports = {
-filterByAge,
-filterByScore,
-filterByServiceRecord,
-sortByScore,
-getMeanAge,
-getMeanAgeReduce,
-getNamesOnly,
+    filterByAge,
+    filterByScore,
+    filterByServiceRecord,
+    sortByScore,
+    getMeanAge,
+    getMeanAgeReduce,
+    getNamesOnly,
 };
